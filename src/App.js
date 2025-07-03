d74068fe7ba5a6d6818431dd34e79560d1b8d37b
Henry <henry.zheng@ar-logistics.com.cn>
2025-07-03 18:15:45 +0800

    fix: 修复GitHub Pages路由问题，将BrowserRouter改为HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { ConfigProvider } from 'antd-mobile';
 import zhCN from 'antd-mobile/es/locales/zh-CN';
 
